e34315a1a5cfa58edda5c68b81fc66b8e1d1dbef
Robin <robin.isaksson@gmail.com>
2018-10-12 21:12:56 +0200

    Add CTA to ajax loader

--- src/js/modules/ajax-loader-module.js
+++ src/js/modules/ajax-loader-module.js
@@ -1,5 +1,5 @@
 // Vendor
-// import TweenLite from 'gsap';
+import TweenLite from 'gsap';
 
 // UTILS
 import {AjaxRequest, EventDispatcher, DOM, DeviceInfo, URLManager} from 'verktyget';
@@ -13,87 +13,53 @@ class AjaxLoaderModule extends EventDispatcher {
 		
 		// Bind event
 		this.onAjaxLoaded = this.onAjaxLoaded.bind(this);
-		// this.onNewPageLoaded = this.onNewPageLoaded.bind(this);
-		// this.onLoadNewPage = this.onLoadNewPage.bind(this);
-		// this.onOldSceneHidden = this.onOldSceneHidden.bind(this);
-		// this.onHistoryChange = this.onHistoryChange.bind(this);
-		
+		this.onClickCTA = this.onClickCTA.bind(this);
 		
 		// query DOM for relevant nodes
 		this.node = document.querySelector('.module.ajax-loading');
+		this.ctaNode = this.node.querySelector('.cta');
+		this.linkNode = this.node.querySelector('.link');
+
+		this.linkNode.addEventListener('click', this.onClickCTA);
+			
+	}
+	
+	onClickCTA(event) {
+		// var url = '/subpage';
+		event.preventDefault();
 		
-		// // URL Handling, AJAX Loading
-		// URLManager.Init();
-    // URLManager.addEventListener('onUrlChange', this.onHistoryChange); // Browser interaction - Back/Forward
-		var url = '/subpage';
+		
+		var url = event.target.href;
 		var ajax = new AjaxRequest(url);
 		ajax.addEventListener('loaded', this.onAjaxLoaded);
 		ajax.load();
+		
 	}
 	
-
 	onAjaxLoaded(event) {
 
-
+		// Hide CTA + animateOut
+		TweenLite.to(this.ctaNode, 0.5, {height:0, opacity:0});
+		
 		// Create new html node
 		var newHtml = event.target.htmlResponse;
 		var newNode = document.createElement('div');
 		newNode.innerHTML = newHtml;
 		this.subpageHTML = newNode.querySelector('.scene');
-		console.log(this.subpageHTML, this.sceneNode);
-		// Add subpage html
-		this.node.appendChild(this.subpageHTML);
 		
-		// // set title
-		// let headTitle = newHtml.match(/<title>([^$]+)<\/title>/);
-		// var title = headTitle ? headTitle[1] : "";
-		// document.title = title;
-		// 
-		// 
-		// // hide current scene
-		// anime({
-		// 	targets: this.sceneNode,
-		// 	opacity: 0,
-		// 	duration: 500,
-		// 	easing: 'linear'
-		// });
-		// 
-		// 
-		// if (this.fromHistory === false) {
-		// 	let obj = { scrollY:this.scroll.y };
-		// 
-		// 	let windowScroll = anime({
-		// 	  targets: obj,
-		// 	  scrollY: 0,
-		// 	  duration: 400,
-		// 		delay: 300,
-		// 		easing: 'easeInQuart',
-		// 		update: (anim) => {
-		// 			window.scrollTo(0, obj.scrollY);
-	  // 		},
-		// 		complete: this.onOldSceneHidden
-		// 	});
-		// } else {
-		// 	window.setTimeout(this.onOldSceneHidden, 700);
-		// }
+		// Add subpage html + animateIn
+		DOM.Style(this.subpageHTML, {opacity:0});
+		this.node.appendChild(this.subpageHTML);
+		TweenLite.to(this.subpageHTML, 0.5, {delay:0.5, opacity:1});
 		
 		this.dispatchEvent({type: 'loaded', target: this});
 	}
-	// onLoaded(event) {
-	// 
-	// 	// Image is loaded
-	// 	this.imageNode.src = event.target.image.src;
-	// 
-	// 	// Dispatch to main
-	// 	this.dispatchEvent({type: 'loaded', target: this});
-	// }
 	
 	
 	setScroll() {}
 	
 	setSize() {
 		// this.size = DeviceInfo.GetSize();
-		// this.imageLoader.updateSize();
 	}
 }
 
